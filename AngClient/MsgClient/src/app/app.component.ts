e6c00578eaaa211d5c1b944279b1365ec4fafbcb
IDB LAB-1 PC -6 <IDB LAB-1 PC -6@DESKTOP-TQDGPQA>
2024-03-02 18:58:50 +0600

    Add project files.

---
+++ AngClient/MsgClient/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'MsgClient';
+
+  private connection!: HubConnection;
+  public user!: string | null;
+  public message: string = "";
+  public Messages: ChatMessage[] = [];
+
+  constructor() {
+    this.connection = new HubConnectionBuilder()
+      .withUrl('https://localhost:7003/chat')
+      .withAutomaticReconnect()
+      .build();
+  }
+
+  ngOnInit(): void {
+    while (!this.user)
+      this.user = prompt('Enter your name');
+
+    this.connection.on('ReceiveText', (user, message) => {
+
+      this.Messages.push(new ChatMessage(user, message));
+    });
+
+    this.connection.on('ReceiveImg', (user, img) => {
+
+      this.Messages.push(new ChatMessage(user, img, false));
+    });
+    
+    this.connection.start();
+    
+  }
+
+  get disconnected(): boolean {
+    return this.connection.state != HubConnectionState.Connected;
+  }
+
+
+  sendMessage() {
+    if (!this.user || !this.message) return;
+    this.connection.invoke('ShareText', this.user, this.message);
+    this.message = '';
+  }
+
+  sendFile(event: any) {
+    const file: File = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.connection.invoke("ShareImg", this.user, reader.result);
+    };
+  }
+  
+}
+
+export class ChatMessage {
+  constructor(public UserName: string, public Message: string, public IsText: boolean = true) { }
+}
---
+++ MessengerApp/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace MessengerApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddSignalR(o=>o.MaximumReceiveMessageSize= 2 * 1024 * 1024);
+			builder.Services.AddCors();
+
+			var app = builder.Build();
+
+			app.UseCors(o=>o
+			.WithOrigins("http://localhost:4200")
+			.AllowAnyHeader()
+			.AllowAnyMethod()
+			.AllowCredentials()
+			);
+
+			app.MapHub<MyHub>("/chat");
+
+			app.Run();
+		}
+
+		public class MyHub : Hub
+		{
+			public void ShareText(string username, string message)
+			{
+				this.Clients.All.SendAsync("ReceiveText", username, message);
+			}
+			public void ShareImg(string username, string img)
+			{
+				this.Clients.All.SendAsync("ReceiveImg", username, img);
+			}
+
+		}
+	}
+}
